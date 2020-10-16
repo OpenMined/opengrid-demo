0154434b5e953129c69cf8d7ceaa5a475dfe1dca
Patrick Cason <me@patrickcason.com>
2020-10-16 10:15:49 +0100

    Attempting suspense with reactfire

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { useFirestoreDocData, useFirestore, SuspenseWithPerf } from 'reactfire';
 
 import { useFirebase } from './firebase';
 import { AppContext } from './context';
@@ -16,24 +17,43 @@ TODO:
 - ... move on with your life
 */
 
-const App = () => {
-  const firebase = useFirebase();
-  const [user, setUser] = useState(firebase.user);
+function Burrito() {
+  // lazy load the Firestore SDK and create a document reference
+  const burritoRef = useFirestore().collection('tryreactfire').doc('burrito');
 
-  useEffect(() => {
-    firebase.auth.onAuthStateChanged((authUser) => {
-      authUser ? setUser(authUser) : setUser(null);
-    });
-  }, [firebase.auth]);
+  // subscribe to the doc. just one line!
+  const burrito = useFirestoreDocData(burritoRef);
+
+  // get the value from the doc
+  const isYummy = burrito.yummy;
+
+  return <p>The burrito is {isYummy ? 'good' : 'bad'}</p>;
+}
+
+const App = () => {
+  // const firebase = useFirebase();
+  // const [user, setUser] = useState(firebase.user);
+
+  // useEffect(() => {
+  //   firebase.auth.onAuthStateChanged((authUser) => {
+  //     authUser ? setUser(authUser) : setUser(null);
+  //   });
+  // }, [firebase.auth]);
 
   return (
-    <AppContext.Provider value={{ user }}>
-      <Router>
-        <Header />
-        <Routes />
-        <ToastContainer position="bottom-left" />
-      </Router>
-    </AppContext.Provider>
+    // <AppContext.Provider value={{ user }}>
+    <Router>
+      <SuspenseWithPerf
+        fallback={'loading burrito status...'}
+        traceId={'load-burrito-status'}
+      >
+        <Burrito />
+      </SuspenseWithPerf>
+      {/* <Header /> */}
+      {/* <Routes /> */}
+      <ToastContainer position="bottom-left" />
+    </Router>
+    // </AppContext.Provider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { unstable_createRoot } from 'react-dom';
 import { ThemeProvider, theme, CSSReset } from '@chakra-ui/core';
 
 import App from './App';
-import Firebase, { FirebaseContext } from './firebase';
+// import Firebase, { FirebaseContext } from './firebase';
+import { FirebaseAppProvider } from 'reactfire';
+
+// const firebase = new Firebase();
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 const root = document.getElementById('root');
-const firebase = new Firebase();
-
-render(
+const WrappedApp = () => (
   <React.StrictMode>
-    <FirebaseContext.Provider value={firebase}>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
       <ThemeProvider theme={theme}>
         <CSSReset />
         <App />
       </ThemeProvider>
-    </FirebaseContext.Provider>
-  </React.StrictMode>,
-  root
+    </FirebaseAppProvider>
+  </React.StrictMode>
 );
+// const WrappedApp = () => (
+//   <React.StrictMode>
+//     <FirebaseContext.Provider value={firebase}>
+//       <ThemeProvider theme={theme}>
+//         <CSSReset />
+//         <App />
+//       </ThemeProvider>
+//     </FirebaseContext.Provider>
+//   </React.StrictMode>
+// );
+
+// Concurrent
+unstable_createRoot(root).render(<WrappedApp />);
+
+// Not concurrent
+// render(<WrappedApp />, root);
