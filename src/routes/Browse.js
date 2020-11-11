6bce695b1682fea3b7880af342eadbdb685d7308
Héricles Emanuel <hericles.me@gmail.com>
2020-11-11 15:13:35 -0300

    create browse page

---
+++ src/routes/Browse.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { Box, Heading } from "@chakra-ui/core";
+import { useFirestore } from "reactfire";
+
+import Data from "../components/Data";
+import Page from "../components/Page";
+import GridContainer from "../components/GridContainer";
+
+export default () => {
+  const [mode, ] = useState("datasets");
+  const [results, setResults] = useState([]);
+  const [itemsPerPage] = useState(20);
+
+  const db = useFirestore();
+
+  useEffect(() => { 
+    db.collection("datasets")
+      .orderBy("upvotes", "desc")
+      .limit(itemsPerPage)
+      .get()
+      .then((collection) => {
+        const items = collection.docs.map((item) => item.data());
+        setResults(items);
+      });
+  }, [itemsPerPage, db]);
+
+  return (
+    <Page title="Browse">
+      <GridContainer isInitial>
+        <Box>
+          {results.length > 0 &&
+            results.map((data, i) => (
+              <Data {...data} mode={mode} key={i} mb={4} />
+            ))}
+          {results.length === 0 && (
+            <Heading
+              as="span"
+              size="md"
+              color="gray.700"
+              display="block"
+              mb={4}
+            >
+              There are no {mode}.
+            </Heading>
+          )}
+        </Box>
+      </GridContainer>
+    </Page>
+  );
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,16 @@
-import React, { lazy } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { useUser } from 'reactfire';
+import React, { lazy } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useUser } from "reactfire";
+import Browse from "./Browse";
 
-const Homepage = lazy(() => import('./Homepage'));
-const EditUser = lazy(() => import('./users/EditUser'));
-const Search = lazy(() => import('./Search'));
-const NewData = lazy(() => import('./data/NewData'));
-const MyData = lazy(() => import('./data/MyData'));
-const Data = lazy(() => import('./data/Data'));
-const EditData = lazy(() => import('./data/EditData'));
-const NoMatch = lazy(() => import('./NoMatch'));
+const Homepage = lazy(() => import("./Homepage"));
+const EditUser = lazy(() => import("./users/EditUser"));
+const Search = lazy(() => import("./Search"));
+const NewData = lazy(() => import("./data/NewData"));
+const MyData = lazy(() => import("./data/MyData"));
+const Data = lazy(() => import("./data/Data"));
+const EditData = lazy(() => import("./data/EditData"));
+const NoMatch = lazy(() => import("./NoMatch"));
 
 const AuthRoute = (props) => {
   const user = useUser();
@@ -22,6 +23,7 @@ export default () => (
     <Route path="/" element={<Homepage />} />
     <AuthRoute path="edit-user" element={<EditUser />} />
     <Route path="search" element={<Search />} />
+    <Route path="browse" element={<Browse />} />
     <Route path="datasets">
       <AuthRoute path="new" element={<NewData />} />
       <AuthRoute path="me" element={<MyData />} />
